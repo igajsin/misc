16e7a17a4e2b17b7189b20aa469fbabb70259cbb
Igor Gajsin <igajsin@mirantis.com>
2015-01-19 19:52:26 +0300

    imporve output in shose.sh

--- shose.sh
+++ shose.sh
@@ -7,6 +7,5 @@ _env () (virsh net-list|grep admin|cut -d " " -f 2)
 _ip () (virsh net-info $1| grep Bridge|awk '{print $2}'|xargs ip a l|grep inet|awk '{print $2}')
 
 for s in $(_env); do
-    ip= _ip $s
-    echo ${s%_admin} $ip
+    echo ${s%_admin}  $(_ip $s)
 done
