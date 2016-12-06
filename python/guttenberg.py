3deba3b2543997ecee5728e23d3c19d91e35455f
Igor Gajsin <igajsin@mirantis.com>
2016-12-06 18:47:13 +0300

    It can make words Trie

--- python/guttenberg.py
+++ python/guttenberg.py
@@ -2,27 +2,26 @@
 # -*- coding: utf-8 -*-
 """Attempt to solve the task from facebook interview."""
 
-cur_c = None
+import argparse
+import logging
+from pathlib import Path
+import sys
 
-words = set()
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
 
 class Trie(object):
     """Trie of x option * (char, trie) list."""
 
-    def __init__(self, char, parent=None):
+    def __init__(self, char):
         """Init Trie with char."""
         self.num = None
         self.char = char
         self.char_of_childs = set()
-        self.parent = parent
 
     def __repr__(self):
         """Pretty print it."""
-        parent = ""
-        if self.parent:
-            parent = "{}:{}".format(self.parent.char, self.parent.num)
-
-        return "({}|{}:{} -> {})".format(parent, self.char, self.num,
+        return "({}:{} -> {})".format(self.char, self.num,
                                          self.char_of_childs)
 
     def childs(self):
@@ -36,14 +35,6 @@ class Trie(object):
     def _find_child(self, c, childs):
         return filter(lambda ch: ch.char == c, childs)[0]
 
-    def add_to_parent(self, n):
-        """Add n to the parent.num."""
-        parent = self.parent
-        if parent.num:
-            parent.num += 1
-        else:
-            parent.num = 1
-
     def add_normal_char(self, c):
         """Add c to the childs."""
         childs = self.childs()
@@ -51,7 +42,7 @@ class Trie(object):
         if c in chars:
             cur_c = self._find_child(c, childs)
         else:
-            new_T = Trie(c, self)
+            new_T = Trie(c)
             self.char_of_childs = self.char_of_childs | set([new_T])
             cur_c = new_T
         return cur_c
@@ -64,24 +55,71 @@ class Trie(object):
             self.num = 1
         return None
 
+    def add_char(self, c):
+        """Add delimter or normal char."""
+        if c.isalnum():
+            return self.add_normal_char(c.lower())
+        else:
+            return self.add_delimiter()
 
-def main():
+
+def find_char(char, words):
+    """Find start-letter between words."""
+    c = filter(lambda w: w.char == char, words)
+    if c:
+        c = c[0]
+    return c
+
+
+def add_str(str, words=set(), log=logging.getLogger()):
+    """Add string to the set of words."""
+    cur_c = None
+    for c in str:
+        if cur_c:  # add letter to the current word
+            cur_c = cur_c.add_char(c)
+        else:  # begin new word
+            cur_c = find_char(c, words)
+            if not cur_c:
+                cur_c = Trie(c)
+                words = words | set([cur_c])
+    else:
+        if cur_c:
+            cur_c.add_delimiter()
+    return words
+
+
+def handle_file(fn, words=set([])):
+    """Extract words from file fn."""
+    f = open(fn, 'r')
+    for line in f:
+        words = add_str(line, words)
+    return words
+
+def words2list(words, lst=[]):
+    for word in words:
+
+
+
+
+def main(fn, num):
     """Main entry point."""
-    pass
+    words = set([])
+    p = Path(fn)
+    if p.is_file() and p.exists():
+        words = handle_file(p.as_posix(), words)
+    else:
+        for f in p.iterdir():
+            if f.exists():
+                words = handle_file(f.as_posix(), words)
+    print words
+    sys.exit(0)
 
 
 if __name__ == "__main__":
-    main()
-
-
-def add_str(str, words=set()):
-    if len(str) == "":
-        return words
-    else:
-        cur_c = None
-        for c in str:
-            if cur_c:  # add letter to the current word
-                pass
-            else:  # begin new word
-                pass
-    return words
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", help=("file/path to dir with files"
+                                      "with text for calculation"))
+    parser.add_argument("-n", "--num", help="amount of most frequent words",
+                        default=10, type=int)
+    args = parser.parse_args()
+    main(args.path, args.num)
